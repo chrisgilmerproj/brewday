87527d9f7bdd1f5de439a2f38fda52b22ac71264
Chris Gilmer <chris.gilmer@gmail.com>
2016-05-30 06:45:04 -0700

    Trade out grain_list for pale

--- tests/test_grains.py
+++ tests/test_grains.py
@@ -1,17 +1,20 @@
 import textwrap
 import unittest
 
-from fixtures import grain_list
+from fixtures import pale
 
 
 class TestGrains(unittest.TestCase):
 
+    def setUp(self):
+        self.grain = pale
+
     def test_str(self):
-        out = str(grain_list[0])
+        out = str(self.grain)
         self.assertEquals(out, 'pale 2-row')
 
     def test_format(self):
-        out = grain_list[0].format()
+        out = self.grain.format()
         msg = textwrap.dedent("""Pale 2-row Grain
                                  ----------------
                                  Color:             2 degL
@@ -20,25 +23,25 @@ class TestGrains(unittest.TestCase):
         self.assertEquals(out, msg)
 
     def test_get_dry_to_liquid_malt_weight(self):
-        out = grain_list[0].get_dry_to_liquid_malt_weight(5.0)
+        out = self.grain.get_dry_to_liquid_malt_weight(5.0)
         self.assertEquals(out, 6.25)
 
     def test_get_liquid_to_dry_malt_weight(self):
-        out = grain_list[0].get_liquid_to_dry_malt_weight(6.25)
+        out = self.grain.get_liquid_to_dry_malt_weight(6.25)
         self.assertEquals(out, 5.0)
 
     def test_get_grain_to_liquid_malt_weight(self):
-        out = grain_list[0].get_grain_to_liquid_malt_weight(5.0)
+        out = self.grain.get_grain_to_liquid_malt_weight(5.0)
         self.assertEquals(out, 3.75)
 
     def test_get_liquid_malt_to_grain_weight(self):
-        out = grain_list[0].get_liquid_malt_to_grain_weight(3.75)
+        out = self.grain.get_liquid_malt_to_grain_weight(3.75)
         self.assertEquals(out, 5.0)
 
     def test_get_specialty_grain_to_liquid_malt_weight(self):
-        out = grain_list[0].get_specialty_grain_to_liquid_malt_weight(5.0)
+        out = self.grain.get_specialty_grain_to_liquid_malt_weight(5.0)
         self.assertEquals(out, 4.45)
 
     def test_get_liquid_malt_to_specialty_grain_weight(self):
-        out = grain_list[0].get_liquid_malt_to_specialty_grain_weight(4.45)
+        out = self.grain.get_liquid_malt_to_specialty_grain_weight(4.45)
         self.assertEquals(out, 5.0)
