5fb40d7390bf268c361d60b828852b5ac8e4692a
Chris Gilmer <chris.gilmer@gmail.com>
2016-06-13 21:20:02 -0700

    Add cereals spider

---
+++ data/scraper/spiders/cereals_spider.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from scraper.items import CerealsItem
+
+
+class BeerSmithSpider(scrapy.Spider):
+    name = "beersmith"
+    allowed_domains = ["beersmith.com"]
+    start_urls = (
+        'http://beersmith.com/grain-list/',
+    )
+
+    def parse(self, response):
+        for href in response.xpath('//table[@class="ms-list4-main"]/tbody/tr/td/a/@href').extract():  # nopep8
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.parse_cereals_contents)
+
+    def parse_cereals_contents(self, response):
+        item = CerealsItem()
+
+        item[u'source'] = response.url
+
+        name = response.xpath('//h2/text()').extract()[0]
+        item[u'name'] = name
+        notes = response.xpath("//body/center/table/tbody/tr/td/text()").extract()  # nopep8
+        item[u'notes'] = ' '.join(notes).strip()
+
+        for entry in response.xpath('//table/tbody/tr/td/table/tbody/tr/td'):
+            category = entry.xpath('b/text()').extract()
+            data = entry.xpath('text()').extract()
+            cat_safe = ' '.join([c.strip() for c in category]).lower().strip()
+            data_safe = ' '.join([d.strip() for d in data])
+            if cat_safe:
+                cat_safe = cat_safe[:-1]
+                cat_safe = cat_safe.replace('/', '_')
+                cat_safe = cat_safe.replace(' ', '_')
+                if cat_safe == u'type':
+                    cat_safe = u'cereal_type'
+                item[cat_safe] = data_safe
+        yield item
