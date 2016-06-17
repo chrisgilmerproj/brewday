6db13af82d6c8ca22282a42af476f7de720acc0f
Chris Gilmer <chris.gilmer@gmail.com>
2016-06-17 10:33:40 -0700

    Add repr to Grain and test repr methods

--- tests/test_grains.py
+++ tests/test_grains.py
@@ -14,6 +14,10 @@ class TestGrains(unittest.TestCase):
         out = str(self.grain)
         self.assertEquals(out, 'Pale 2-row')
 
+    def test_repr(self):
+        out = repr(self.grain)
+        self.assertEquals(out, "Grain('pale 2-row', short_name='2-row', color=2, hot_water_extract=0.76)")  # nopep8
+
     def test_format(self):
         out = self.grain.format()
         msg = textwrap.dedent("""\
@@ -56,6 +60,10 @@ class TestGrainAdditions(unittest.TestCase):
     def test_str(self):
         out = str(self.grain_add)
         self.assertEquals(out, 'Pale 2-row, 95 %')
+
+    def test_repr(self):
+        out = repr(self.grain_add)
+        self.assertEquals(out, "GrainAddition(Grain('pale 2-row', short_name='2-row', color=2, hot_water_extract=0.76), percent_malt_bill=95)")  # nopep8
 
     def test_format(self):
         out = self.grain_add.format()
