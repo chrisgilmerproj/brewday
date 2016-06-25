c64a0d6d3708d402ab1144822880026fc7218d21
Chris Gilmer <chris.gilmer@gmail.com>
2016-06-25 11:53:29 -0700

    Break out utilities into smaller files

---
+++ brew/utilities/color.py
@@ -0,0 +1,196 @@
+
+from ..constants import GAL_PER_LITER
+from ..constants import IMPERIAL_UNITS
+from ..constants import POUND_PER_KG
+from ..constants import SI_UNITS
+from ..validators import validate_units
+
+
+def srm_to_ebc(srm):
+    """
+    Convert SRM to EBC Color
+    """
+    return srm * 1.97
+
+
+def ebc_to_srm(ebc):
+    """
+    Convert EBC to SRM Color
+    """
+    return ebc / 1.97
+
+
+def calculate_mcu(grain_weight, beer_color, final_volume,
+                  units=IMPERIAL_UNITS):
+    """
+    Return MCU
+
+    grain_weight - in lbs or kg
+    beer_color - in deg Lovibond
+    final_volume - in gal or liters
+
+    http://beersmith.com/blog/2008/04/29/beer-color-understanding-srm-lovibond-and-ebc/
+    """  # nopep8
+    validate_units(units)
+    if units == SI_UNITS:
+        grain_weight = grain_weight * POUND_PER_KG
+        final_volume = final_volume * GAL_PER_LITER
+
+    mcu = grain_weight * beer_color / final_volume
+    return mcu
+
+
+def calculate_srm_mosher(grain_weight, beer_color, final_volume,
+                         units=IMPERIAL_UNITS):
+    """
+    Mosher Equation
+
+    grain_weight - in lbs or kg
+    beer_color - in deg Lovibond
+    final_volume - in gal or liters
+    """  # nopep8
+    mcu = calculate_mcu(grain_weight, beer_color, final_volume, units=units)
+    if mcu < 7.0:
+        raise Exception("Mosher equation does not work for MCU < 7.0")
+    srm = (mcu * 0.3) + 4.7
+    return srm
+
+
+def calculate_srm_daniels(grain_weight, beer_color, final_volume,
+                          units=IMPERIAL_UNITS):
+    """
+    Daniels Equation
+
+    grain_weight - in lbs or kg
+    beer_color - in deg Lovibond
+    final_volume - in gal or liters
+    """  # nopep8
+    mcu = calculate_mcu(grain_weight, beer_color, final_volume, units=units)
+    if mcu < 11.0:
+        raise Exception("Daniels equation does not work for MCU < 11.0")
+    srm = (mcu * 0.2) + 8.4
+    return srm
+
+
+def calculate_srm_daniels_power(grain_weight, beer_color, final_volume,
+                                units=IMPERIAL_UNITS):
+    """
+    Daniels Power Equation based on work by Druey
+
+    grain_weight - in lbs or kg
+    beer_color - in deg Lovibond
+    final_volume - in gal or liters
+    """  # nopep8
+    mcu = calculate_mcu(grain_weight, beer_color, final_volume, units=units)
+    srm = 1.73 * (mcu ** 0.64) - 0.27
+    if srm > 50.0:
+        raise Exception("Daniels Power equation does not work above SRM 50")
+    return srm
+
+
+def calculate_srm_noonan_power(grain_weight, beer_color, final_volume,
+                               units=IMPERIAL_UNITS):
+    """
+    Noonan Power Equation based on work by Druey
+
+    grain_weight - in lbs or kg
+    beer_color - in deg Lovibond
+    final_volume - in gal or liters
+    """  # nopep8
+    mcu = calculate_mcu(grain_weight, beer_color, final_volume, units=units)
+    srm = 15.03 * (mcu ** 0.27) - 15.53
+    if srm > 50.0:
+        raise Exception("Noonan Power equation does not work above SRM 50")
+    return srm
+
+
+def calculate_srm_morey_hybrid(grain_weight, beer_color, final_volume,
+                               units=IMPERIAL_UNITS):
+    """
+    A hybrid approach used by Morey.  Assumptions:
+
+    1. SRM is approximately equal to MCU for values from 0 to 10.
+    2. Homebrew is generally darker than commercial beer.
+    3. Base on the previous qualitative postulate, I assumed that Ray Daniels'
+       predicted relationship exists for beers with color greater than 10.
+    4. Since Mosher's equation predicts darker color than Daniels' model for
+       values of MCU greater than 37, I assumed that Mosher's approximation
+       governed beer color for all values more than 37 MCUs.
+    5. Difference in color for beers greater than 40 SRM are essentially
+       impossible to detect visually; therefore, I limited the analysis to SRM
+       of 50 and less.
+
+    http://babblehomebrewers.com/attachments/article/61/beercolor.pdf
+    """
+    mcu = calculate_mcu(grain_weight, beer_color, final_volume,
+                        units=units)
+    if 0 < mcu < 10:
+        return mcu
+    elif 10 <= mcu < 37:
+        return calculate_srm_daniels(grain_weight, beer_color, final_volume,
+                                     units=units)
+    elif 37 <= mcu < 50:
+        return calculate_srm_mosher(grain_weight, beer_color, final_volume,
+                                    units=units)
+    else:
+        raise Exception("Morey Hybrid does not work above SRM 50")
+
+
+def calculate_srm_morey(grain_weight, beer_color, final_volume,
+                        units=IMPERIAL_UNITS):
+    """
+    Morey Equation
+    http://www.morebeer.com/brewingtechniques/beerslaw/morey.html
+
+    grain_weight - in lbs or kg
+    beer_color - in deg Lovibond
+    final_volume - in gal or liters
+
+    http://beersmith.com/blog/2008/04/29/beer-color-understanding-srm-lovibond-and-ebc/
+    """  # nopep8
+    mcu = calculate_mcu(grain_weight, beer_color, final_volume, units=units)
+    srm = 1.4922 * (mcu ** 0.6859)
+    if srm > 50.0:
+        raise Exception("Morey equation does not work above SRM 50")
+    return srm
+
+
+def calculate_srm(grain_weight, beer_color, final_volume,
+                  units=IMPERIAL_UNITS):
+    """
+    General srm calculation uses the Morey Power Equation
+    """
+    return calculate_srm_morey(grain_weight, beer_color, final_volume,
+                               units=units)
+
+
+def lovibond_to_srm(lovibond):
+    """
+    Convert deg Lovibond to SRM
+    https://en.wikipedia.org/wiki/Standard_Reference_Method
+    """
+    return 1.3546 * lovibond - 0.76
+
+
+def srm_to_lovibond(srm):
+    """
+    Convert SRM to deg Lovibond
+    https://en.wikipedia.org/wiki/Standard_Reference_Method
+    """
+    return (srm + 0.76) / 1.3546
+
+
+def srm_to_a430(srm, dilution=1.0):
+    """
+    Get attenuation at A430 from SRM and dilution
+    https://en.wikipedia.org/wiki/Standard_Reference_Method
+    """
+    return srm / (12.7 * dilution)
+
+
+def ebc_to_a430(ebc, dilution=1.0):
+    """
+    Get attenuation at A430 from EBC and dilution
+    https://en.wikipedia.org/wiki/Standard_Reference_Method
+    """
+    return ebc / (25 * dilution)
---
+++ brew/utilities/sugar.py
@@ -0,0 +1,144 @@
+
+from ..constants import HYDROMETER_ADJUSTMENT_TEMP
+from ..constants import IMPERIAL_UNITS
+from ..constants import SI_UNITS
+from ..validators import validate_units
+from .temperature import celsius_to_fahrenheit
+
+
+def sg_to_gu(sg):
+    return (sg - 1) * 1000.0
+
+
+def gu_to_sg(gu):
+    return 1 + (gu / 1000.0)
+
+
+def plato_to_sg(deg_plato):
+    """
+    Specific Gravity (S.G.)
+    S.G. is the density of a liquid or solid compared to that of water.
+    The simple formula for S.G. is:
+
+    S.G. = 1 + 0.004 x Plato
+
+    The more precise calculation of S.G. is:
+
+    S.G. = [(Plato) / (258.6 - (Plato/258.2 x 227.1))] + 1
+
+    Source:
+    http://www.learntobrew.com/page/1mdhe/Shopping/Beer_Calculations.html
+    """
+    return (deg_plato / (258.6 - ((deg_plato / 258.2) * 227.1))) + 1.0
+
+
+def sg_to_plato(sg):
+    """
+    Plato
+    Degrees Plato is the weight of the extract in a 100gram solution at
+    64 degrees Fahrenheit.
+
+    Plato = [(S.G. - 1) x 1000] / 4
+
+    The more precise calculation of Plato is:
+
+    Plato = -616.868 + 1111.14 * sg - 630.272 * sg ** 2 + 135.997 * sg ** 3
+
+    Source:
+    http://www.brewersfriend.com/2012/10/31/on-the-relationship-between-plato-and-specific-gravity/
+    """
+    # return (sg - 1.0) * 1000 / 4
+    return ((135.997*sg - 630.272)*sg + 1111.14)*sg - 616.868
+
+
+def brix_to_sg(brix):
+    """
+    Source:
+    Brew Your Own Magazine
+    http://www.brewersfriend.com/brix-converter/
+    """
+    return (brix / (258.6 - ((brix / 258.2) * 227.1))) + 1
+
+
+def sg_to_brix(sg):
+    """
+    Source:
+    http://en.wikipedia.org/wiki/Brix
+    http://www.brewersfriend.com/brix-converter/
+    """
+    if sg > 1.17874:
+        raise Exception("Above 40 degBx this function no longer works")
+    return (((182.4601 * sg - 775.6821) * sg + 1262.7794) * sg - 669.5622)
+
+
+def brix_to_plato(brix):
+    """
+    The difference between the degBx and degP as calculated from the respective
+    polynomials is:
+
+        degP - degBx = (((-2.81615*sg + 8.79724)*sg - 9.1626)*sg + 3.18213)
+
+    The difference is generally less than +/-0.0005 degBx or degP with the
+    exception being for weak solutions.
+
+    https://en.wikipedia.org/wiki/Brix
+    """
+    return sg_to_plato(brix_to_sg(brix))
+
+
+def plato_to_brix(plato):
+    return sg_to_brix(plato_to_sg(plato))
+
+
+def hydrometer_adjustment(sg, temp, units=IMPERIAL_UNITS):
+    """
+    Adjust for the temperature if it deviates from 59degF.
+
+    http://hbd.org/brewery/library/HydromCorr0992.html
+    Correction(@59F) = 1.313454 - 0.132674*T + 2.057793e-3*T**2 - 2.627634e-6*T**3
+        where T is in degrees F.
+
+    Sources:
+    http://www.brewersfriend.com/hydrometer-temp/
+    http://www.topdownbrew.com/SGCorrection.html
+    http://hbd.org/brewery/library/HydromCorr0992.html
+    http://www.primetab.com/formulas.html
+    """  # nopep8
+    validate_units(units)
+    if units == SI_UNITS:
+        if temp < 0.0 or 100.0 < temp:
+            raise Exception("Correction does not work outside temps 0 - 100C")
+        temp = celsius_to_fahrenheit(temp)
+    elif units == IMPERIAL_UNITS:
+        if temp < 0.0 or 212.0 < temp:
+            raise Exception("Correction does not work outside temps 0 - 212F")
+
+    if temp == HYDROMETER_ADJUSTMENT_TEMP:
+        return sg
+
+    correction = (1.313454 - 0.132674 * (temp ** 1) +
+                  (2.057793 * 10 ** -3) * (temp ** 2) -
+                  (2.627634 * 10 ** -6) * (temp ** 3))
+    return sg + (correction * 0.001)
+
+
+def refractometer_adjustment(og, fg):
+    """
+    Adjust for the presence of alcohol in the refractometer reading
+
+    NOTE: This calculation assumes using Brix or Plato, so the input will be
+    converted from SG to Plato and then converted back.
+
+    Returns: Final Gravity
+
+    Sources:
+    http://seanterrill.com/2011/04/07/refractometer-fg-results/
+    """
+    og_brix = sg_to_brix(og)
+    fg_brix = sg_to_brix(fg)
+
+    new_fg = (1.0000 -
+              0.0044993 * og_brix + 0.011774 * fg_brix +
+              0.00027581 * (og_brix ** 2) - 0.0012717 * (fg_brix ** 2) -
+              0.0000072800 * (og_brix ** 3) + 0.000063293 * (fg_brix ** 3))
+    return brix_to_sg(new_fg)
