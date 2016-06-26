fbb73dc3e4a3da10011f1250fcc52fa24a106a8b
Chris Gilmer <chris.gilmer@gmail.com>
2016-06-26 10:19:50 -0700

    Remove target_ibu from recipe

--- examples/pale_ale.py
+++ examples/pale_ale.py
@@ -48,8 +48,7 @@ if __name__ == "__main__":
                   percent_brew_house_yield=0.70,  # %
                   start_volume=7.0,  # G
                   final_volume=5.0,  # G
-                  target_ibu=40.0)
-
+                  )
     beer.format()
     # Efficiency 70%
     # Boil: 7.0 Gal
