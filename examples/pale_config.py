ee0d66ded04f74a9389077bc7e3729b4cf040a24
Chris Gilmer <chris.gilmer@gmail.com>
2016-07-07 09:38:06 -0700

    Abstract useful pieces of code

--- examples/pale_config.py
+++ examples/pale_config.py
@@ -11,83 +11,99 @@ from brew.recipes import Recipe
 from brew.utilities.sugar import sg_to_gu
 from brew.yeasts import Yeast
 
-recipe = {
-    'name': 'Pale Ale',
-    'start_volume': 7.0,
-    'final_volume': 5.0,
-    'grains': [
-        {'name': 'pale malt 2-row us',
-         'weight': 13.96},
-        {'name': 'caramel crystal malt 20l',
-         'weight': 0.78},
-    ],
-    'hops': [
-        {'name': 'centennial',
-         'weight': 0.57,
-         'boil_time': 60.0},
-        {'name': 'cascade us',
-         'weight': 0.76,
-         'boil_time': 5.0},
-    ],
-    'yeast': {
-        'name': 'Danstar',
-    },
-}
+
+def format_name(name):
+    return name.replace(' ', '_').replace('-', '_')
 
 
-def parse_cereals():
+def read_json_file(filename):
+    data = None
+    with open(filename, 'r') as data_file:
+        data = json.loads(data_file.read())
+    return data
+
+
+def parse_cereals(recipe, data_dir):
     # Look up cereal data
-    cereal_dir = os.path.abspath(os.path.join(os.getcwd(), 'data/cereals/'))
+    cereal_dir = os.path.join(data_dir, 'cereals/')
     cereal_list = [cereal[:-5] for cereal in os.listdir(cereal_dir)]
 
     # Create Grains
     grain_additions = []
     for cereal_data in recipe['grains']:
-        name = cereal_data['name'].replace(' ', '_').replace('-', '_')
+        name = format_name(cereal_data['name'])
         if name in cereal_list:
             cereal_filename = os.path.join(cereal_dir, '{}.json'.format(name))
-            with open(cereal_filename, 'r') as cereal_file:
-                grain_json = json.loads(cereal_file.read())
-                grain = Grain(grain_json['name'],
-                              color=float(grain_json['color'][:-4]),
-                              ppg=sg_to_gu(float(grain_json['potential'][:-3])))  # nopep8
-                grain_add = GrainAddition(grain, weight=float(cereal_data['weight']))  # nopep8
-                grain_additions.append(grain_add)
+            grain_json = read_json_file(cereal_filename)
+            grain = Grain(grain_json['name'],
+                          color=float(grain_json['color'][:-4]),
+                          ppg=sg_to_gu(float(grain_json['potential'][:-3])))  # nopep8
+            grain_add = GrainAddition(grain, weight=float(cereal_data['weight']))  # nopep8
+            grain_additions.append(grain_add)
         else:
-            print 'Cereal not found: {}'.format(name)
+            print('Cereal not found: {}'.format(name))
 
     return grain_additions
 
 
-def parse_hops():
+def parse_hops(recipe, data_dir):
     # Look up hop data
-    hop_dir = os.path.abspath(os.path.join(os.getcwd(), 'data/hops/'))
+    hop_dir = os.path.join(data_dir, 'hops/')
     hop_list = [hop[:-5] for hop in os.listdir(hop_dir)]
 
     # Create Grains
     hop_additions = []
     for hop_data in recipe['hops']:
-        name = hop_data['name'].replace(' ', '_').replace('-', '_')
+        name = format_name(hop_data['name'])
         if name in hop_list:
             hop_filename = os.path.join(hop_dir, '{}.json'.format(name))
-            with open(hop_filename, 'r') as hop_file:
-                hop_json = json.loads(hop_file.read())
-                alpha_acids = float(hop_json['alpha_acid_composition'].split('%')[0]) / 100.  # nopep8
-                hop = Hop(hop_json['name'],
-                          percent_alpha_acids=alpha_acids)
-                hop_add = HopAddition(hop,
-                                      weight=float(hop_data['weight']),
-                                      boil_time=hop_data['boil_time'])
-                hop_additions.append(hop_add)
+            hop_json = read_json_file(hop_filename)
+            alpha_acids = float(hop_json['alpha_acid_composition'].split('%')[0]) / 100.  # nopep8
+            hop = Hop(hop_json['name'],
+                      percent_alpha_acids=alpha_acids)
+            hop_add = HopAddition(hop,
+                                  weight=float(hop_data['weight']),
+                                  boil_time=hop_data['boil_time'])
+            hop_additions.append(hop_add)
         else:
-            print 'Hop not found: {}'.format(name)
+            print('Hop not found: {}'.format(name))
     return hop_additions
 
 
+def parse_yeast(recipe, data_dir):
+    # yeast_dir = os.path.join(data_dir, 'yeast/')
+    return Yeast(recipe['yeast']['name'])
+
+
 def main():
-    grain_additions = parse_cereals()
-    hop_additions = parse_hops()
-    yeast = Yeast(recipe['yeast']['name'])
+
+    recipe = {
+        'name': 'Pale Ale',
+        'start_volume': 7.0,
+        'final_volume': 5.0,
+        'grains': [
+            {'name': 'pale malt 2-row us',
+             'weight': 13.96},
+            {'name': 'caramel crystal malt 20l',
+             'weight': 0.78},
+        ],
+        'hops': [
+            {'name': 'centennial',
+             'weight': 0.57,
+             'boil_time': 60.0},
+            {'name': 'cascade us',
+             'weight': 0.76,
+             'boil_time': 5.0},
+        ],
+        'yeast': {
+            'name': 'Danstar',
+        },
+    }
+
+    data_dir = os.path.abspath(os.path.join(os.getcwd(), 'data/'))
+    grain_additions = parse_cereals(recipe, data_dir)
+    hop_additions = parse_hops(recipe, data_dir)
+    yeast = parse_yeast(recipe, data_dir)
 
     beer = Recipe(recipe['name'],
                   grain_additions=grain_additions,
@@ -97,6 +113,7 @@ def main():
                   final_volume=recipe['final_volume'],
                   )
     beer.format()
+
 
 if __name__ == "__main__":
     main()
