a960678ae9dee5862ef5419910bb00248ed12b47
Chris Gilmer <chris.gilmer@gmail.com>
2016-08-13 06:42:12 -0700

    Update to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='brewday',
-    version='0.0.1',
+    version='0.0.2',
     author='Chris Gilmer',
     author_email='chris.gilmer@gmail.com',
     maintainer='Chris Gilmer',
