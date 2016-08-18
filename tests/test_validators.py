5d0a35bcff11979df30c59089cc5fc0d311c488e
Chris Gilmer <chris.gilmer@gmail.com>
2016-08-18 09:36:47 -0700

    Add tests for validators

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -6,6 +6,8 @@ from brew.validators import validate_grain_type
 from brew.validators import validate_hop_type
 from brew.validators import validate_percentage
 from brew.validators import validate_units
+from brew.validators import validate_required_fields
+from brew.validators import validate_optional_fields
 
 
 class TestValidators(unittest.TestCase):
@@ -44,3 +46,41 @@ class TestValidators(unittest.TestCase):
     def test_validate_units_raises(self):
         with self.assertRaises(Exception):
             validate_units('bad')
+
+    def test_validate_required_fields(self):
+        data = {'required': 'data'}
+        required_fields = [('required', str)]
+        validate_required_fields(data, required_fields)
+
+    def test_validate_required_fields_missing_field_raises(self):
+        data = {'missing': 'data'}
+        required_fields = [('required', str)]
+        with self.assertRaises(Exception):
+            validate_required_fields(data, required_fields)
+
+    def test_validate_required_fields_wrong_field_type_raises(self):
+        data = {'required': 'data'}
+        required_fields = [('required', int)]
+        with self.assertRaises(Exception):
+            validate_required_fields(data, required_fields)
+
+    def test_validate_optional_fields(self):
+        data = {'data': {'optional': 'data'}}
+        optional_fields = [('optional', str)]
+        validate_optional_fields(data, optional_fields)
+
+    def test_validate_optional_fields_extra_data(self):
+        data = {'data': {'extra': 'data'}}
+        optional_fields = [('optional', str)]
+        validate_optional_fields(data, optional_fields)
+
+    def test_validate_optional_fields_missing_data_field(self):
+        data = {'missing': {'optional': 'data'}}
+        optional_fields = [('optional', str)]
+        validate_optional_fields(data, optional_fields)
+
+    def test_validate_optional_fields_wrong_field_type_raises(self):
+        data = {'data': {'optional': 'data'}}
+        optional_fields = [('optional', int)]
+        with self.assertRaises(Exception):
+            validate_optional_fields(data, optional_fields)
