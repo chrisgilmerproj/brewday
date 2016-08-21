7fe3e1841f368f489bd61614ab360d3664310e0c
Chris Gilmer <chris.gilmer@gmail.com>
2016-08-20 21:17:32 -0700

    Convert temp utility and add tests

---
+++ brew/cli/temp.py
@@ -0,0 +1,11 @@
+
+from brew.utilities.temperature import fahrenheit_to_celsius
+from brew.utilities.temperature import celsius_to_fahrenheit
+
+
+def get_temp_conversion(fahrenheit, celsius):
+    if fahrenheit:
+        out = fahrenheit_to_celsius(fahrenheit)
+    elif celsius:
+        out = celsius_to_fahrenheit(celsius)
+    return round(out, 1)
