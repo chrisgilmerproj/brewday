542169ae83194201983d1964ecc65b064dfa7e5f
Chris Gilmer <chris.gilmer@gmail.com>
2016-08-22 09:02:31 -0700

    Test temp parser

--- tests/test_cli_temp.py
+++ tests/test_cli_temp.py
@@ -1,6 +1,7 @@
 
 import unittest
 
+from brew.cli.temp import get_parser
 from brew.cli.temp import get_temp_conversion
 
 
@@ -17,3 +18,27 @@ class TestCliTemp(unittest.TestCase):
     def test_get_sugar_conversion_celsius(self):
         out = get_temp_conversion(None, self.c)
         self.assertEquals(out, self.f)
+
+
+class TestCliArgparserTemp(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = get_parser()
+
+    def test_get_parser_celsius(self):
+        args = ['-c', '25.0']
+        out = self.parser.parse_args(args)
+        expected = {
+            'celsius': 25.0,
+            'fahrenheit': None,
+        }
+        self.assertEquals(out.__dict__, expected)
+
+    def test_get_parser_fahrenheit(self):
+        args = ['-f', '62.0']
+        out = self.parser.parse_args(args)
+        expected = {
+            'celsius': None,
+            'fahrenheit': 62.0,
+        }
+        self.assertEquals(out.__dict__, expected)
