ef659b35da94a3f588f255f4cb2a780e370661a5
Chris Gilmer <chris.gilmer@gmail.com>
2016-09-21 09:24:35 -0700

    Start adding tests for yeast cli

---
+++ tests/test_cli_yeast.py
@@ -0,0 +1,30 @@
+
+import unittest
+
+from brew.cli.yeast import get_parser
+
+
+class TestCliArgparserTemp(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = get_parser()
+        self.default_args = {
+            'cells': 100,
+            'days': 0,
+            'fv': 5.0,
+            'method': 'stir plate',
+            'model': 'white',
+            'num': 1,
+            'og': 1.05,
+            'sg': 1.036,
+            'sv': 0.5283443537159779,
+            'target_pitch_rate': 1.42,
+            'type': 'liquid',
+            'units': 'imperial'}
+
+    def test_get_parser_(self):
+        args = []
+        out = self.parser.parse_args(args)
+        expected = {}
+        self.default_args.update(expected)
+        self.assertEquals(out.__dict__, self.default_args)
