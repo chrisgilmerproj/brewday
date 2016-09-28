e3d95033e2bc1f4941ca5b611b4aaec51d4a462b
Chris Gilmer <chris.gilmer@gmail.com>
2016-09-28 09:45:37 -0700

    Document temperature utilities

--- brew/utilities/temperature.py
+++ brew/utilities/temperature.py
@@ -8,7 +8,11 @@ __all__ = [
 
 def fahrenheit_to_celsius(temp):
     """
-    Convert degrees Fahrenheit  to degrees Celsius
+    Convert degrees Fahrenheit to degrees Celsius
+
+    :param float temp: The temperature in Fahrenheit
+    :return: The temperature in Celsius
+    :rtype: float
     """
     return (temp - 32.0) / 1.8
 
@@ -16,5 +20,9 @@ def fahrenheit_to_celsius(temp):
 def celsius_to_fahrenheit(temp):
     """
     Convert degrees Celsius to degrees Fahrenheit
+
+    :param float temp: The temperature in Celsius
+    :return: The temperature in Fahrenheit
+    :rtype: float
     """
     return(temp * 1.8) + 32.0
