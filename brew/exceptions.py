ec9d97f7017939651fc78605fc81a2f030f88b5f
Chris Gilmer <chris.gilmer@gmail.com>
2017-04-29 21:12:28 -0700

    Add exceptions file

---
+++ brew/exceptions.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    u'BrewdayException',
+    u'DataLoaderException',
+    u'GrainException',
+    u'HopException',
+    u'StyleException',
+    u'YeastException',
+]
+
+
+class BrewdayException(Exception):
+    pass
+
+
+class DataLoaderException(BrewdayException):
+    pass
+
+
+class GrainException(BrewdayException):
+    pass
+
+
+class HopException(BrewdayException):
+    pass
+
+
+class StyleException(BrewdayException):
+    pass
+
+
+class YeastException(BrewdayException):
+    pass
