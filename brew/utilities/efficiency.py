3486d3cb7122ba59c64d9af5b6eb6b12bc97e193
Chris Gilmer <chris.gilmer@gmail.com>
2017-04-30 09:32:09 -0700

    Clean up brew house yield calculator to deal with DME and LME additions

--- brew/utilities/efficiency.py
+++ brew/utilities/efficiency.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from ..constants import GRAIN_TYPE_DME
+from ..constants import GRAIN_TYPE_LME
 from .sugar import sg_to_gu
 
 __all__ = [
@@ -23,6 +25,13 @@ def calculate_brew_house_yield(wort_volume, sg, grain_additions):
     A better measure is to look at just the grains that needed to be steeped
     seperately and measure the specific gravity of that process.
     """
+    grain_adds_lme_dme = filter(lambda grain_add: grain_add.grain_type
+                                in [GRAIN_TYPE_DME, GRAIN_TYPE_LME],
+                                grain_additions)
+    grain_adds_other = filter(lambda grain_add: grain_add.grain_type
+                              not in [GRAIN_TYPE_DME, GRAIN_TYPE_LME],
+                              grain_additions)
 
-    gravity_units = sum([grain_add.gu for grain_add in grain_additions])
-    return sg_to_gu(sg) * wort_volume / gravity_units
+    gu_lme_dme = sum([grain_add.gu for grain_add in grain_adds_lme_dme])
+    gu_other = sum([grain_add.gu for grain_add in grain_adds_other])
+    return (sg_to_gu(sg) * wort_volume - gu_lme_dme) / gu_other
