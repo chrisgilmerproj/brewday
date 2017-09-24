e3c14e572c170ac68b8d13f5d171883f784b6f41
Chris Gilmer <chris.gilmer@gmail.com>
2017-09-24 07:56:37 -0700

    Small fixes to rest schedule

--- examples/enzymatic_rests.py
+++ examples/enzymatic_rests.py
@@ -11,8 +11,8 @@ def main():
     print("Enzymatic rests schedule")
 
     liquor_to_grist_ratio = 1.5  # qt:lbs
-    grain_weight = 2.0  # lbs
-    water_volume = 2.0 * liquor_to_grist_ratio  # qt
+    grain_weight = 8.0  # lbs
+    water_volume = grain_weight * liquor_to_grist_ratio  # qt
 
     print("")
     print("Starting with {} lbs of grain".format(grain_weight))
@@ -44,15 +44,21 @@ def main():
     initial_temp = target_temp
     target_temp = 158
     infusion_temp = 210
+    water_volume += infusion_volume
 
     infusion_volume = mash_infusion(target_temp, initial_temp,
-                                    grain_weight, water_volume + infusion_volume,  # noqa
+                                    grain_weight, water_volume,
                                     infusion_temp=infusion_temp)
 
     print("")
     print("Add {} qts of {} degF water".format(round(infusion_volume, 1), infusion_temp))  # noqa
     print("Your temperature should then reach {} degF".format(target_temp))
     print("Keep your temperature here for 20 minutes")
+    print("")
+
+    water_volume += infusion_volume
+    print("You should now have {} qts of water".format(round(water_volume, 1)))
+    print("Now remove the grains and continue with brewing")
 
 
 if __name__ == "__main__":
