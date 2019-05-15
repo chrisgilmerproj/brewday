689e0190cbf5f928c0dc069cbfc851fac301ab24
Chris Gilmer <chris.gilmer@gmail.com>
2019-05-15 12:40:51 -0700

    Add python 3.8 and 3.7. Remove python 3.3 from testing

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ setup(
     download_url="https://github.com/chrisgilmerproj/brewday/tarball/{}".format(
         VERSION
     ),  # noqa
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    packages=find_packages(exclude=[
+        "*.tests",
+        "*.tests.*",
+        "tests.*",
+        "tests"]),
     entry_points={
         "console_scripts": [
             "abv = brew.cli.abv:main",
@@ -49,10 +53,11 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
